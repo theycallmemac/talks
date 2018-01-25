1e99facf6f60b363dce64c1a126c8a1dfd7c721f
James Mc Dermott <james.mcdermott7@mail.dcu.ie>
2018-01-25 10:56:51 +0000

    Add files via upload

---
+++ Web/ComputeTY/writeContent.js
@@ -0,0 +1,2 @@
+document.write("<h1>This is a heading</h1>");
+document.write("<p>This is a paragraph</h1>");
---
+++ Web/ComputeTY/writeContent2.js
@@ -0,0 +1,4 @@
+window.onload = function writeContent() {
+    document.write("<h1>This is a heading</h1>");
+    document.write("<p>This is a paragraph</h1>");
+}
